749ce2bfd835a7792bf79e7147124dfbe10f609a
sarawut <sanit.sa@outlook.com>
2021-12-01 00:57:02 +0700

    update bsc mainnet rpc

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -7,7 +7,7 @@ export const TEST = false
 export const ADMIN_ADDRESS = TEST ? "0x8813bd2dA50a1Fb95443a41d36Fd19D0FAF6E593" : "0xB722A8Af023A92Bc1764978fD926cDE1b48C5e28"
 
 export const AVALANCHE_URL = TEST ? 'https://api.avax-test.network/ext/bc/C/rpc' : 'https://api.avax.network/ext/bc/C/rpc'
-export const BSC_URL = TEST ? "https://data-seed-prebsc-1-s1.binance.org:8545" : 'https://bsc-dataseed.binance.org/'
+export const BSC_URL = TEST ? "https://data-seed-prebsc-1-s1.binance.org:8545" : 'https://speedy-nodes-nyc.moralis.io/e4bc1614104e7bb02f6bd9f9/bsc/mainnet'
 
 export const BRIDGE_AVAX_ADDRESS = TEST ? "0xFdD7C285838c3DD8584BFcC7bEB1259ac658d17e" : "0xf572a9Bc621b23969D7232f1717eA2E63fE0A5DE"
 export const MKC_AVAX_ADDRESS = TEST ? "0xE82c4ce37F381242E9082c28c84936778dFCc1D3" : "0xCA490D74e3e7044481a61A76f8f9995994e8DdFD"
